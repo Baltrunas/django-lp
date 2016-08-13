5dadfad5d6239921113c2062627c37653e2421fc
Stanislav Baltrunas <stanislav@baltrunas.ru>
2016-08-13 13:30:57 +0600

    upload_to

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 from django.utils.translation import ugettext as _
 from django.db import models
 
-from ..pages.models import Page
+from helpful.fields import upload_to
 
+from ..pages.models import Page
 
 class Block(models.Model):
 	title = models.CharField(verbose_name=_('Title'), max_length=256)
@@ -10,8 +11,8 @@ class Block(models.Model):
 	slug = models.SlugField(verbose_name=_('Slug'), max_length=128)
 	sub_title = models.CharField(verbose_name=_('Sub Title'), max_length=256, blank=True, null=True)
 	text = models.TextField(verbose_name=_('Text'), blank=True, null=True)
-	image = models.FileField(verbose_name=_('Image'), upload_to='block/image', blank=True, null=True)
-	bg = models.FileField(verbose_name=_('Background'), upload_to='block/bg', blank=True, null=True)
+	image = models.FileField(verbose_name=_('Image'), upload_to=upload_to, blank=True, null=True)
+	bg = models.FileField(verbose_name=_('Background'), upload_to=upload_to, blank=True, null=True)
 
 	# template = models.CharField(_('Template'), max_length=124, blank=True, null=True)
 
@@ -23,6 +24,9 @@ class Block(models.Model):
 	created_at = models.DateTimeField(verbose_name=_('Created At'), auto_now_add=True)
 	updated_at = models.DateTimeField(verbose_name=_('Updated At'), auto_now=True)
 
+
+	# def dependent_from(self):
+	# 	return self.content_object
 
 	def public_subblocks(self):
 		return self.subblocks.filter(public=True)
@@ -41,7 +45,7 @@ class SubBlock(models.Model):
 	block = models.ForeignKey(Block, verbose_name=_('Block'), related_name='subblocks')
 	sub_title = models.CharField(verbose_name=_('Sub Title'), max_length=256, blank=True, null=True)
 	description = models.TextField(verbose_name=_('Description'), blank=True, null=True)
-	image = models.FileField(verbose_name=_('Image'), upload_to='subblock/image', blank=True, null=True)
+	image = models.FileField(verbose_name=_('Image'), upload_to=upload_to, blank=True, null=True)
 
 	order = models.PositiveSmallIntegerField(verbose_name=_('Sort ordering'), default=500)
 
@@ -63,11 +67,11 @@ class FAQ(models.Model):
 
 	problem_title = models.CharField(verbose_name=_('Problem Title'), max_length=256)
 	problem_description = models.TextField(verbose_name=_('Problem Description'), blank=True, null=True)
-	problem_image = models.FileField(verbose_name=_('Problem Image'), upload_to='faq/image', blank=True, null=True)
+	problem_image = models.FileField(verbose_name=_('Problem Image'), upload_to=upload_to, blank=True, null=True)
 
 	solutions_title = models.CharField(verbose_name=_('Solutions Title'), max_length=256)
 	solutions_description = models.TextField(verbose_name=_('Solutions Description'), blank=True, null=True)
-	solutions_image = models.FileField(verbose_name=_('Solutions Image'), upload_to='faq/image', blank=True, null=True)
+	solutions_image = models.FileField(verbose_name=_('Solutions Image'), upload_to=upload_to, blank=True, null=True)
 
 	order = models.PositiveSmallIntegerField(verbose_name=_('Sort ordering'), default=500)
 
